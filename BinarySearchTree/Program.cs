dcb877ab6f9eb3b17ae93ea155b81c6b25fc60f6
srk-333 <sauravmk333@gmail.com>
2022-01-31 11:13:15 +0530

    [Saurav]Add:Welcome message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Program");
+            Console.ReadLine();
+        }
+    }
+}
