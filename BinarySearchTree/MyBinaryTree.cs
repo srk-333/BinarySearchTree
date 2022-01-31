c1ed1f2d49c6a1cdab2c94e4584255e23393fce4
srk-333 <sauravmk333@gmail.com>
2022-01-31 11:57:39 +0530

    [Saurav]Add:Program to Create a Simple BST

---
+++ BinarySearchTree/MyBinaryTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class MyBinaryTree<T> where T : IComparable
+    {
+        public INode<T> root;
+        /// <summary>
+        /// Creates the node.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void CreateNode(T value)
+        {
+            INode<T> newNode = new INode<T>(value);
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                INode<T> parent = root;
+                INode<T> current = root;
+                if (current.data.CompareTo(value) >= 0)
+                {
+                    parent.leftTree = newNode;
+                    return;
+                }
+                else
+                {
+                    parent.rightTree = newNode;
+                    return;
+                }
+            }
+        }
+        /// <summary>
+        /// Displays Nodes in BST
+        /// </summary>
+        public void Display()
+        {
+            INode<T> temp = root;
+            if (temp != null)
+            {
+                Console.WriteLine("Root Node:{0}", temp.data.ToString());
+            }
+            if (temp.leftTree != null)
+            {
+                Console.WriteLine("Left Node:{0}", temp.leftTree.data.ToString());
+            }
+            if (temp.rightTree != null)
+            {
+                Console.WriteLine("Right Node:{0}", temp.rightTree.data.ToString());
+            }
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,7 +11,20 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Program");
+            ViewBST();
             Console.ReadLine();
+        }
+        /// <summary>
+        /// Method to Initialize Nodes.
+        /// </summary>
+        public static void ViewBST()
+        {
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+            tree.CreateNode(56);
+            tree.CreateNode(30);
+            tree.CreateNode(70);
+            Console.WriteLine("Display Nodes present in Binary Search Tree");
+            tree.Display();
         }
     }
 }
